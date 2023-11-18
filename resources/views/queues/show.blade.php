34b04e2485efa4954fe6206de2cea05d3b30a08f
Emmanuel Arcamo <click2edit@gmail.com>
2023-11-18 12:22:20 +0800

    printing the queue updated

--- resources/views/queues/show.blade.php
+++ resources/views/queues/show.blade.php
@@ -11,10 +11,26 @@
 <body class="antialiased">
     <div class="max-w-7xl mx-auto p-6 lg:p-8">
         <div class="flex justify-center">
-            <p>Queue ID: {{ $queue->id }}</p>
+            <p>Queue ID: <span id="queueNumber">{{ $queue->id }}</span></p>
             <!-- You can add more details or customize the display as needed -->
+
+            <!-- Button to trigger print -->
+            <button onclick="printQueueNumber()" class="bg-blue-500 text-white px-4 py-2 rounded">Print Queue
+                Number</button>
         </div>
     </div>
+
+    <script>
+        function printQueueNumber() {
+            var queueNumber = document.getElementById('queueNumber').innerText;
+            var printWindow = window.open('', '_blank');
+            printWindow.document.write('<html><head><title>Queue Number</title></head><body>');
+            printWindow.document.write('<p>Queue ID: ' + queueNumber + '</p>');
+            printWindow.document.write('</body></html>');
+            printWindow.document.close();
+            printWindow.print();
+        }
+    </script>
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -864,7 +864,7 @@
             <br>
             <h1>Welcome to the Queuing System</h1>
             <br>
-            <a href="#"
+            <a href="queues/create"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 role="button">Queue Now</a>
         </div>
